59040bf36ec774029e8411bbbd3c1b28d575aec5
Aswad <aswad29972gmail.com>
2026-06-30 20:57:55 +0500

    bash script changed memcached.sh

--- scripts/memcache.sh
+++ scripts/memcache.sh
@@ -11,11 +11,19 @@ sudo systemctl start memcached
 
 sed -i 's/127.0.0.1/0.0.0.0/g' /etc/sysconfig/memcached
 
+sudo ss -tulnp | grep 11211
+
+sudo fuser -k 11211/tcp 2>/dev/null
+
 sudo systemctl restart memcached
 
 firewall-cmd --add-port=11211/tcp --permanent
 
 firewall-cmd --reload
+
+sudo ss -tulnp | grep 11211
+
+sudo fuser -k 11211/tcp 2>/dev/null
 
 systemctl restart memcached
 
